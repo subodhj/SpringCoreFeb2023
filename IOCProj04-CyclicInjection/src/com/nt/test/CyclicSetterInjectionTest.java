47db9d9a367038ea64d7cbd627ddcd7430022f31
Subodh <welcome@DESKTOP-4BFJCBM>
2023-02-27 16:39:50 +0530

    replaced XmlBeanFactory container with DefaultListableBeanFactory container.

--- IOCProj04-CyclicInjection/src/com/nt/test/CyclicSetterInjectionTest.java
+++ IOCProj04-CyclicInjection/src/com/nt/test/CyclicSetterInjectionTest.java
@@ -1,8 +1,7 @@
 package com.nt.test;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 
 import com.nt.beans.A;
 
@@ -10,12 +9,17 @@ public class CyclicSetterInjectionTest {
 
 	public static void main(String[] args) {
 
-		BeanFactory factory = null;
-
+//		BeanFactory factory = null;
+		DefaultListableBeanFactory factory = null;
+		XmlBeanDefinitionReader reader = null;
 		A a = null;
 
 		// create ioc container
-		factory = new XmlBeanFactory(new FileSystemResource("src/com/nt/cfgs/applicationContext.xml"));
+//		factory = new XmlBeanFactory(new FileSystemResource("src/com/nt/cfgs/applicationContext.xml"));
+
+		factory = new DefaultListableBeanFactory();
+		reader = new XmlBeanDefinitionReader(factory);
+		reader.loadBeanDefinitions("com/nt/cfgs/applicationContext.xml");
 
 		// get target class object
 		a = (A) factory.getBean("a1");
