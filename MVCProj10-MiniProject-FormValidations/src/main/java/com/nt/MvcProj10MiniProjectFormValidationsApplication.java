239994078b2f6dd2c2cfb48e7b99f5d8dfea39de
Subodh <welcome@DESKTOP-4BFJCBM>
2023-08-19 20:06:28 +0530

    Tried to add @ProperySource in cfg class but not working.
    So no changes done.

--- MVCProj10-MiniProject-FormValidations/src/main/java/com/nt/MvcProj10MiniProjectFormValidationsApplication.java
+++ MVCProj10-MiniProject-FormValidations/src/main/java/com/nt/MvcProj10MiniProjectFormValidationsApplication.java
@@ -2,8 +2,10 @@ package com.nt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+//@PropertySource("classpath:com/nt/commons/validation.properties")
 public class MvcProj10MiniProjectFormValidationsApplication {
 
 	public static void main(String[] args) {
